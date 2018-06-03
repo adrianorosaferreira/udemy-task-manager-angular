e04087efa7bb826e907d12a47207d6d307577b73
Adriano Ferreira <adrianorosaferreira@gmail.com>
2018-06-03 18:29:04 -0300

    Getting params from TaskDetailComponent route

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -1,17 +1,32 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params} from '@angular/router';
+
+import 'rxjs/add/operator/switchMap';
 
 import { Task } from '../shared/task.model';
+import { TaskService } from '../shared/task.service';
 
 @Component({
     selector: 'task-detail',
     templateUrl: './task-detail.component.html'
 })
 
-export class TaskDetailComponent {
+export class TaskDetailComponent implements OnInit {
     // Este @Input permite que um outro component possa acessar esta propriedade
     @Input() public task: Task;
 
-    public constructor() {
+    public constructor(
+        private taskService: TaskService,
+        private route: ActivatedRoute
+    ) {}
 
+    public ngOnInit() {
+        // Sempre quando utilizo um Observable eu preciso usar um subscrib
+        this.route.params
+            .switchMap((params: Params) =>
+                // o '+' converte para mumero
+                this.taskService.getTask(+params['id'])
+            )
+            .subscribe(task => this.task = task );
     }
 }
