30b2beedff3bb518b83ee2092beeb09e96c9468d
Adriano Ferreira <adrianorosaferreira@gmail.com>
2018-06-06 22:35:00 -0300

    Fixing branch adding-http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,12 @@
 import { Component } from '@angular/core';
-import { LearningObservables } from './learning-observables.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
-  providers: [ LearningObservables ]
+  styleUrls: ['./app.component.css']
 })
 
 export class AppComponent {
   title = 'Gerenciador de Tarefas!';
-
-  public constructor(private learningObservables: LearningObservables) {
-
-  }
 }
 
