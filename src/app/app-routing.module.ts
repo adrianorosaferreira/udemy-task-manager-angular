ff717049ecd5aa9cdf090c5561ef3eee541bfe93
Adriano Ferreira <adrianorosaferreira@gmail.com>
2018-06-16 16:53:02 -0300

    Adding SignUpFormComponent base template and ajustments

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignUpFormComponent } from './sign-up-form/sign-up-form.component';
 import { TasksComponent } from './tasks/tasks.component';
 import { TaskDetailComponent } from './tasks/task-detail/task-detail.component';
 
 const ROUTES = RouterModule.forRoot([
   { path: 'tasks/:id', component: TaskDetailComponent },
   { path: 'dashboard', component: DashboardComponent },
+  { path: 'sign-up', component: SignUpFormComponent },
   { path: 'tasks', component: TasksComponent },
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'}
 ]);
--- src/app/sign-up-form/sign-up-form.component.ts
+++ src/app/sign-up-form/sign-up-form.component.ts
@@ -14,12 +14,12 @@ export class SignUpFormComponent {
             name: [null, [Validators.required, Validators.minLength(7), Validators.maxLength(100)]],
             email: [null, [Validators.required, Validators.email]],
             password: [null, [Validators.required, Validators.minLength(8)]],
-            password_confirmation: [null, [Validators.required]]
+            passwordConfirmation: [null, [Validators.required]]
         });
     }
 
     public signUpUser() {
-        console.log("Formulario de SignUp Enviado");
+        console.log('Formulario de SignUp Enviado');
         console.log(this.userForm.value);
     }
 }
