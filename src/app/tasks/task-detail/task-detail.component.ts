276f6cc9eccf16e58a0ac48178d381b2b8a6a630
Adriano Ferreira <adrianorosaferreira@gmail.com>
2018-06-15 21:13:46 -0300

    Refactoring the TaskDetailComponent

--- src/app/tasks/task-detail/task-detail.component.ts
+++ src/app/tasks/task-detail/task-detail.component.ts
@@ -10,7 +10,8 @@ import { TaskService } from '../shared/task.service';
 
 @Component({
     selector: 'task-detail',
-    templateUrl: './task-detail.component.html'
+    templateUrl: './task-detail.component.html',
+    styles: ['.form-control-feedback{ margin-right:20px }']
 })
 
 export class TaskDetailComponent implements OnInit, AfterViewInit {
@@ -41,7 +42,6 @@ export class TaskDetailComponent implements OnInit, AfterViewInit {
       });
     }
 
-
     public ngOnInit() {
       this.task = new Task(null, null);
       // Sempre quando utilizo um Observable eu preciso usar um subscrib
@@ -54,30 +54,26 @@ export class TaskDetailComponent implements OnInit, AfterViewInit {
           );
     }
 
-
     public setTask(task: Task): void {
       this.task = task;
       this.reactiveTaskForm.patchValue(task);
     }
 
-
     public ngAfterViewInit() {
       $('#deadline').datetimepicker({
         'sideBySide': true,
         'locale': 'pt-br'
-      }).on('dp.change', () => this.reactiveTaskForm.get('deadline').setValue($('#deadline').val()));
+      }).on('dp.change', () => this.getField('deadline').setValue($('#deadline').val()));
     }
-
 
     public goBack() {
         this.location.back();
     }
 
-
     public update() {
-      this.task.title = this.reactiveTaskForm.get('title').value;
-      this.task.deadline = this.reactiveTaskForm.get('deadline').value;
-      this.task.done = this.reactiveTaskForm.get('done').value;
+      this.task.title = this.getField('title').value;
+      this.task.deadline = this.getField('deadline').value;
+      this.task.done = this.getField('done').value;
       this.task.description = this.reactiveTaskForm.get('description').value;
 
       this.taskService.update(this.task)
@@ -93,11 +89,24 @@ export class TaskDetailComponent implements OnInit, AfterViewInit {
     // Forms errors methods
     public fieldClassForErrorOrSuccess(fieldName: string) {
       return {
-        'has-error' : this.showFieldError(this.reactiveTaskForm.get('title')),
-        'has-success' : this.reactiveTaskForm.get('title').valid
+        'has-error' : this.showFieldError(fieldName),
+        'has-success' : this.getField(fieldName).valid
       };
     }
-    public showFieldError(field): boolean {
+
+    public iconClassErroOrSuccess(fieldName: string) {
+      return {
+        'glyphicon-ok': this.getField(fieldName).valid,
+        'glyphicon-remove': this.showFieldError(fieldName)
+      }
+    }
+
+    public showFieldError(fieldName: string): boolean {
+      let field = this.getField(fieldName);
       return field.invalid && (field.touched || field.dirty);
+    }
+
+    public getField(fieldName: string) {
+      return this.reactiveTaskForm.get(fieldName);
     }
 }
